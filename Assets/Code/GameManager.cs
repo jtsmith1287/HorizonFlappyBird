aa2a894f0f1c2de406b8b086e0bd4fb1abfd9769
Justin Smith <jtsmith1287@gmail.com>
2015-05-04 21:42:45 -0700

    Moving logs and gamemanager

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject ObstaclePrefab;
+	GameObject[] ObstacleArray = new GameObject[10];
+
+	// Use this for initialization
+	void Start() {
+
+		for (int i = ObstacleArray.Length; i < ObstacleArray.Length; i++) {
+			ObstacleArray[i] = Instantiate(ObstaclePrefab);
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
