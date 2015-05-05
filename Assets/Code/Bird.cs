7ef63ebcd462d7f2cfca0d2006548ceb28e59cae
Justin Smith <jtsmith1287@gmail.com>
2015-05-04 21:15:15 -0700

    Initial commit;

---
+++ Assets/Code/Bird.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bird : MonoBehaviour {
+
+	public KeyCode FlapKey;
+	public int FlapStrength;
+
+	public Sprite FlapWingsUp;
+	public Sprite FlapWingsDown;
+
+	public Rigidbody2D BirdPhysics;
+
+	// Update is called once per frame
+	void Update() {
+
+		if (Input.GetKeyDown(FlapKey)) {
+			FlapWings();
+		}
+	}
+
+	void FlapWings() {
+
+		BirdPhysics.velocity = new Vector2(0, FlapStrength);
+	}
+}
---
+++ Assets/Code/ScrollingBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	[Header("Scrolling Background Data")]
+	public Material BackgroundMat;
+	[Range(0.01f, 0.3f)]
+	public float ScrollSpeed = 0.1f;
+	public Vector2 TextureOffset;
+
+	// Update is called once per frame
+	void Update() {
+
+		TextureOffset.x += Time.deltaTime * ScrollSpeed;
+		BackgroundMat.mainTextureOffset = TextureOffset;
+	}
+}
